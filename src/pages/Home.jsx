af37462ae684b30fdac6a1cf54fb3551d5efc663
anandbaraik <anandbaraik2014@gmail.com>
2023-07-09 21:59:14 +0530

    fix: home ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ import CommonCard from "../components/CommonCard";
 const Home = () => {
     const {continents} = useTripAdvisor();
     return (
-      <Flex m={2} flexDir="column" h="full" alignItems="center" gap={2}>
-        <Flex flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-            <Heading>
+      <Flex m={2} mt={8} flexDir="column" h="full" alignItems="center" gap={2}>
+        <Flex flexDirection={'column'} mb={8} justifyContent={'center'} alignItems={'center'} gap={5}>
+            <Heading as="h1" size="2xl">
               Welcom to Trip Advisor
               <ColorModeSwitcher/>
             </Heading>
-            <Text color="blue">Top continents for your next holiday</Text>
+            <Heading as="h2" size="lg" color="purple.400">Top continents for your next holiday</Heading>
         </Flex>
         <Flex flexWrap={'wrap'} gap={5}>
           {
